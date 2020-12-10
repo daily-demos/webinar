a860271b3c0462f54bdffd4531873b935e379443
Jessica Mitchell <jess@pluot.co>
2020-12-10 12:12:24 -0500

    update slash images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,8 +15,8 @@ function App() {
     <Router>
       <Container>
         <ImageContainer>
-          <BackgroundImg1 src={decorativeSlash1} alt=" " />
-          <BackgroundImg2 src={decorativeSlash2} alt=" " />
+          <BackgroundImg1 src={decorativeSlash2} alt=" " />
+          <BackgroundImg2 src={decorativeSlash1} alt=" " />
           <BackgroundImg3 src={decorativeSlash3} alt=" " />
         </ImageContainer>
         <Body>
